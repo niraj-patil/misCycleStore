5ac8a6896fb3a44f9347b4dc36238b63edbeb11a
niraj-patil <niraj.anilpatil@gmail.com>
2022-02-27 20:24:34 +0530

    Models Added

--- cycleStore/cycleStoreApp/models.py
+++ cycleStore/cycleStoreApp/models.py
@@ -1,30 +1,56 @@
 from email.headerregistry import Address
 from django.db import models
 
-Gender=(
+GENDER=(
         ('F','Female'),
         ('M','Male'),
-        ('O','Other'))
-Order_Status=(
+        ('O','Other')
+        )
+ORDER_STATUS=(
     ('processing','Processing'),
-    ('Shipment','Shipment Scheduled'),
-    ('Shipped','Shipped'),
-    ('delivered','out of delivery'),
-    ('canclled','cancle')
+    ('shipment','Shipment Scheduled'),
+    ('shipped','Shipped'),
+    ('delivered','Out for Delivery'),
+    ('cancelled','Cancel')
     )
+AGE_GROUP=(
+    ('0 to 14','Below 14'),
+    ('15 to 26','15 to 24'),
+    ('24 to 36','24 to 36'),
+    ('36+','Above 36')
+)
+
 
 # Create your models here.
+class Colour(models.Model):
+    colour          = models.CharField(max_length=20,primary_key=True)
+class Type(models.Model):
+    type            = models.CharField(max_length=20,primary_key=True)
 class Product(models.Model):
-    Id              =models.IntegerField(primary_key=True)
+    ID              = models.AutoField(primary_key=True)
     title           = models.CharField(max_length=120)
     description     = models.TextField()
-    price           = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    quantity        = models.IntegerField()
+    price           = models.DecimalField(decimal_places=2, max_digits=20, default=4999)
+    quantity        = models.PositiveIntegerField(default=50)
     image           = models.ImageField(upload_to='static', null=True, blank=True)
     featured        = models.BooleanField(default=False)
     active          = models.BooleanField(default=True)
-    timestamp       = models.DateTimeField(auto_now_add=True)
-
+    sales           = models.PositiveIntegerField(default=0)
+    profit          = models.DecimalField(decimal_places=2, max_digits=30,default=0)
+    timestamp       = models.DateTimeField(auto_now_add=True,auto_now=False)
+    
+class UniqueProduct(models.Model):
+    product         =models.ForeignKey(Product,on_delete=models.CASCADE)
+    type            =models.ForeignKey(Type,on_delete=models.CASCADE)
+    colour          =models.ForeignKey(Colour,on_delete=models.CASCADE)
+    profitPerItem   =models.DecimalField(decimal_places=2, max_digits=20,default=1000)
+    sales           =models.IntegerField(default=0)
+    profit          =models.DecimalField(decimal_places=2, max_digits=20,default=0)
+    
+    def save(self, *args, **kwargs):
+        self.profit=self.sales * self.profitPerItem
+        super().save(*args, **kwargs)
+    
 class Address(models.Model):
     address_line_1  = models.CharField(max_length=120)
     address_line_2  = models.CharField(max_length=120, null=True, blank=True)
@@ -33,24 +59,25 @@ class Address(models.Model):
     state           = models.CharField(max_length=120)
     postal_code     = models.CharField(max_length=120)
 
-
 class Customer(models.Model):
-    Id              =models.IntegerField(primary_key=True)
-    Name            =models.CharField(max_length=20)
-    billingAddress  =models.ForeignKey(Address,on_delete=models.CASCADE)
-    age             =models.IntegerField()
-    Gender          =models.CharField(max_length=1,choices=Gender)
-    Email           =models.EmailField(max_length=50)
-
-
+    ID              =models.AutoField(primary_key=True)
+    name            =models.CharField(max_length=20)
+    username        =models.CharField(max_length=20)
+    password        =models.CharField(max_length=20)
+    billingAddress  =models.ForeignKey(Address,on_delete=models.CASCADE,blank=True)
+    age             =models.CharField(max_length=8,choices=AGE_GROUP)
+    gender          =models.CharField(max_length=1,choices=GENDER)
+    email           =models.EmailField(max_length=50)
+    
 class Order(models.Model):
-    Id              =models.IntegerField(primary_key=True)
-    ProductID       =models.ForeignKey(Product,on_delete=models.CASCADE)
-    Buyer           =models.ForeignKey(Customer,on_delete=models.DO_NOTHING)
-    ShippingAddress =models.ForeignKey(Address,on_delete=models.CASCADE)
-    Status          =models.CharField(max_length=15,choices=Order_Status)
-    TrasactionID    =models.CharField(max_length=50)
-    timestamp       = models.DateTimeField(auto_now_add=True)
+    ID              = models.AutoField(primary_key=True)
+    product         = models.ForeignKey(UniqueProduct,on_delete=models.DO_NOTHING)
+    customer        = models.ForeignKey(Customer,on_delete=models.DO_NOTHING)
+    shippingAddress = models.ForeignKey(Address,on_delete=models.DO_NOTHING)
+    status          = models.CharField(max_length=15,choices=ORDER_STATUS)
+    transactionID   = models.CharField(max_length=50)
+    timestamp       = models.DateTimeField(auto_now_add=True,auto_now=False)
+
 
 class Cart(models.Model):
     user        = models.ForeignKey(Customer, null=True, blank=True,on_delete=models.CASCADE)
@@ -59,4 +86,3 @@ class Cart(models.Model):
     total       = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
-    total1      = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
